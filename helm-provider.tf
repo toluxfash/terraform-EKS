1965f18dd66285da35d91090308c144190ecfec9
fash <toluxfashs@gmail.com>
2025-08-25 14:11:07 +0100

    chore: pin provider versions and fix kubernetes/helm provider syntax

--- helm-provider.tf
+++ helm-provider.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
-provider "helm" {
+provider "helm"  {
   kubernetes {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
